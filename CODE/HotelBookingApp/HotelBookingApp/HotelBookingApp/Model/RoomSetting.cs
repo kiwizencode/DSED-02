798a5d2da612a4403afae21aa1d58720f0870292
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-06-20 15:31:23 +1200

    save changes

---
+++ CODE/HotelBookingApp/HotelBookingApp/HotelBookingApp/Model/RoomSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBookingApp.Model
+{
+    public class RoomSetting
+    {
+        public int Room_FK { get; set; }
+        public string Description { get; set; }
+        public float Cost { get; set; }
+        public float Max { get; set; }
+        public int Count { get; set; }
+    }
+}
