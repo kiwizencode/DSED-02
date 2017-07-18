716b9d0b3128924aef97b948aa677efff8c86f15
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-07-18 13:22:04 +1200

    create a new project

---
+++ CODE/V2.0/HotelBookingApp/HotelBookingApp/HotelBookingApp/Model/Abstract_Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBookingApp.Model
+{
+    public abstract class Abstract_Model
+    {
+        public Guid ID_PK;
+        //public string ID_PK;
+    }
+}
